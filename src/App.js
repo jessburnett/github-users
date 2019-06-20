98afe2ecb25540e6be4463ebc1e64c81145f091f
jessburnett <jess.a.burnett@gmail.com>
2019-06-20 09:04:01 -0400

    init && cleanup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return ( 
+    <div className = "App" >
+      <div>
+        <h2 > Github finder </h2> 
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
